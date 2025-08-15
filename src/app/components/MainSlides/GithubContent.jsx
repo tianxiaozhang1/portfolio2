27a633fceec7108db0b5f92763bee733e412f174
Tianxiao Zhang <tianxiaozhang@gmail.com>
2025-08-15 15:22:24 -0400

    Fix

--- src/app/components/MainSlides/GithubContent.jsx
+++ src/app/components/MainSlides/GithubContent.jsx
@@ -15,13 +15,13 @@ function GithubContent({ theme }) {
             <div className={`${theme === 'dark' ? 'text-sky-200' : 'text-[#3271AE]'}`}>description: <span className={`${theme === 'dark' ? 'text-stone-200' : 'text-gray-700'}`}>&nbsp;Explore my coding journey, open-source contributions, and a diverse range of personal projects.</span></div>
             <div className={`${theme === 'dark' ? 'text-stone-200' : 'text-gray-700'} text-start`}>&#x2c;</div>
             
-            <div className={`${theme === 'dark' ? 'text-sky-200' : 'text-[#3271AE]'} flex`}>key_stats:<span className={`${theme === 'dark' ? 'text-[#fad91c]' : 'text-[#DB9D34]'} `}>&nbsp;&#123;</span></div>
+            {/* <div className={`${theme === 'dark' ? 'text-sky-200' : 'text-[#3271AE]'} flex`}>key_stats:<span className={`${theme === 'dark' ? 'text-[#fad91c]' : 'text-[#DB9D34]'} `}>&nbsp;&#123;</span></div>
             <div className={`${theme === 'dark' ? 'text-sky-200' : 'text-[#3271AE]'} flex flex-col ml-6`}>
                 <div className='flex'>repositories: "30+"<div className={`${theme === 'dark' ? 'text-stone-200' : 'text-gray-700'}`}>&#x2c;</div></div>
                 <div className='flex'>contributions: "Consistent commits and activity"<div className={`${theme === 'dark' ? 'text-stone-200' : 'text-gray-700'}`}>&#x2c;</div></div>
                 <div className='flex'>languages: "Python, JavaScript, TypeScript, HTML, CSS"</div>
             </div>
-            <div className={`${theme === 'dark' ? 'text-[#fad91c]' : 'text-[#DB9D34]'} text-start ml-2`}>&nbsp;&#125;<span className={`${theme === 'dark' ? 'text-stone-200' : 'text-gray-700'}`}>&#x2c;</span></div> 
+            <div className={`${theme === 'dark' ? 'text-[#fad91c]' : 'text-[#DB9D34]'} text-start ml-2`}>&nbsp;&#125;<span className={`${theme === 'dark' ? 'text-stone-200' : 'text-gray-700'}`}>&#x2c;</span></div>  */}
 
             {/* <div className={`${theme === 'dark' ? 'text-sky-200' : 'text-[#3271AE]'} flex`}>featured_projects:<span className={`${theme === 'dark' ? 'text-[#fad91c]' : 'text-[#DB9D34]'} `}>&nbsp;&#123;</span></div>
             <div className={`${theme === 'dark' ? 'text-sky-200' : 'text-[#3271AE]'} flex flex-col ml-6`}>
